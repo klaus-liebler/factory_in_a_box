6b69b51e635b6acc5eb402fddc7d4e2258cd8499
Klaus Liebler <mail@klaus-liebler.de>
2026-07-27 15:18:58 +0200

    Deleted historic stuff

--- firmware_stm32_control_unit/test/mocks/log.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cstdio>
-
-#define log_debug(fmt, ...) std::printf("[debug] " fmt "\n", __VA_ARGS__)
--- firmware_stm32_control_unit/test/mocks/stm32g4xx_hal.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-#define __HAL_RCC_TIM17_CLK_ENABLE() ((void)0)
-
-inline void NVIC_SetPriority(int, uint32_t) {}
-inline void NVIC_EnableIRQ(int) {}
